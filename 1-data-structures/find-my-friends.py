b08f7863ce8aefe4dc84773896e382b408b2e49a
Kat <21105989+ksolis3197@users.noreply.github.com>
2024-03-21 17:15:22 -0400

    Create find-my-friends.py

---
+++ 1-data-structures/find-my-friends.py
@@ -0,0 +1,13 @@
+# Task 1: Create Friends' Location Tuples
+friend1_location = (37.7749, -122.4194) # San Francisco
+friend2_location = (40.7128, -74.0060) # New York
+friend3_location = (4.624335, -74.063644) # Bogota
+
+# Bonus: Accessing Tuple Elements
+print("Latitude and Longitude of the furthest friend's location:", friend1_location)
+
+# Task 2: Tuple Operations
+all_friends_locations = friend1_location + friend2_location + friend3_location
+
+# Task 3: Print
+print("All friends' locations combined:", all_friends_locations)
