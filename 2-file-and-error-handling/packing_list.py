0ccb7d34beef1043acb3b8fa9df0027a7d70720c
Kat <21105989+ksolis3197@users.noreply.github.com>
2024-03-21 18:11:39 -0400

    Create packing_list.py

---
+++ 2-file-and-error-handling/packing_list.py
@@ -0,0 +1,21 @@
+import csv
+
+data = [
+  ['Item', 'Quantity'],
+  ['Mugs', 2],
+  ['Hangers', 30],
+  ['Shoes', 2]
+]
+
+try:
+  with open('packing_list.csv', 'r') as file:
+    csv_reader = csv.reader(file)
+
+    for row in csv_reader:
+      print(row)
+except FileNotFoundError:
+  print('Packing list file not found. Creating a new one.')
+  with open('packing_list.csv', 'w') as file:
+    csv_writer = csv.writer(file)
+
+    csv_writer.writerows(data)
