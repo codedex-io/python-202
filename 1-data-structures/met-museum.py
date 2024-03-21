4b7a858d587327d2f0227700f21066d99480033d
Kat <21105989+ksolis3197@users.noreply.github.com>
2024-03-21 17:20:58 -0400

    Create met-museum.py

---
+++ 1-data-structures/met-museum.py
@@ -0,0 +1,9 @@
+# Dictionary representing a Pyxis
+pyxis = {
+    'artist': 'Penthesilea painter',
+    'period': 'classical',
+		'date': '465-460 BCE',
+    'culture': ['Greek', 'Attic'],
+		'medium': ['terracotta', 'white-ground'],
+    'dimensions': {'height': '4.75in', 'height w/cover': '6.75in'}
+}
