ccec4b88e7de31b742b7afce1a067cab5f8e4be4
Kat <21105989+ksolis3197@users.noreply.github.com>
2024-04-09 16:39:10 -0400

    Create 11_full_circle.py
    adding solution for 11

---
+++ 3-functional-programming/11_full_circle.py
@@ -0,0 +1,10 @@
+def circle_area(radius):
+    #Pure function to calculate the area of a circle.
+    return 3.14 * radius ** 2
+
+# Test the Pure function
+radius_of_circle = 5
+area_of_circle = circle_area(radius_of_circle)
+
+# Print to prove result
+print("Area of the circle:", area_of_circle)
