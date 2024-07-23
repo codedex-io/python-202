2e6f7f6f7be441a2d7344c8062e6df8e550ecf1b
Ellie Popoca <estrellapopoca2002@gmail.com>
2024-07-23 18:52:43 -0400

    Update 14_mental_math.py solution

--- 3-functional-programming/14_mental_math.py
+++ 3-functional-programming/14_mental_math.py
@@ -1,14 +1,12 @@
 # Mental Math 🧮
 # Codédex
 
-# Define the range of integers
-start = 1
-end = 11
-numbers_range = range(start, end)
+# Define the list of numbers
+numbers = [57, 10, 82, 36, 89, 46, 13, 23, 92, 48]
 
 # Create a list of even numbers using list comprehension
-even_numbers = [num for num in numbers_range if num % 2 == 0]
+even_numbers = [num for num in numbers if num % 2 == 0]
 
 # Display the original range and the list of even numbers
-print('Original Range:', list(numbers_range))
+print('Original Range:', list(numbers))
 print('Even Numbers:', even_numbers)
