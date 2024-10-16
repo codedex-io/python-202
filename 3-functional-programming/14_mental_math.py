1da517dde765fec6fa8887cc7bb43cf7ca60579d
Akıner Kısa <88156124+akinerkisa@users.noreply.github.com>
2024-10-16 23:56:38 +0300

    Update 14_mental_math.py
    Bonus: create and print a list of odd numbers
     / Solution updated with bonus task.

--- 3-functional-programming/14_mental_math.py
+++ 3-functional-programming/14_mental_math.py
@@ -7,6 +7,10 @@ numbers = [57, 10, 82, 36, 89, 46, 13, 23, 92, 48]
 # Create a list of even numbers using list comprehension
 even_numbers = [num for num in numbers if num % 2 == 0]
 
+# List comprehension to filter odd numbers (Bonus)
+odd_numbers = [num for num in numbers if num % 2 != 0]
+
 # Display the original range and the list of even numbers
 print('Original Range:', list(numbers))
 print('Even Numbers:', even_numbers)
+print("Odd Numbers:", odd_numbers) # (Bonus)
