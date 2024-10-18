a8afc7cbd6fcfc2419784c593108b62f4b13c655
IMDOINGLearning <dereklogan210@gmail.com>
2024-10-18 09:15:06 -0400

    Update string_utils.py
    put in is_capitalized() function and also wrote s[0].isupper() which checks if the first character is capitalized

--- 4-testing/17-assertions/string_utils.py
+++ 4-testing/17-assertions/string_utils.py
@@ -7,8 +7,5 @@ def reverse_string(s):
 def capitalize_string(s):
   return s.capitalize()
 
-def has_capital_letters(s):
-  for char in s:
-    if char.isupper():
-      return True
-  return False
+def is_capitalized(s):
+  return s[0].isupper()
