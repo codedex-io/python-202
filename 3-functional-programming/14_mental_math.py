cf915c464f8fd78ee00c96c3f8a91d6f5ae53ab2
Bruno Viola <99220989+BrunoViola@users.noreply.github.com>
2024-11-01 23:51:20 +0000

    fix: remove unnecessary list() function

--- 3-functional-programming/14_mental_math.py
+++ 3-functional-programming/14_mental_math.py
@@ -8,5 +8,5 @@ numbers = [57, 10, 82, 36, 89, 46, 13, 23, 92, 48]
 even_numbers = [num for num in numbers if num % 2 == 0]
 
 # Display the original range and the list of even numbers
-print('Original Range:', list(numbers))
+print('Original Range:', numbers)
 print('Even Numbers:', even_numbers)
