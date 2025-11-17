5dd8df9b909ea38f60711b8db197f0ec5a5dbfac
Sonny Li <sonnynomnom@gmail.com>
2025-11-17 16:42:15 -0500

    Update 01_hi_bestie.py

--- 1-data-structures/01_hi_bestie.py
+++ 1-data-structures/01_hi_bestie.py
@@ -1,31 +1,16 @@
 # Hi Bestie 👋
 # Codédex
 
-# Define a list with slang terms
-genz_slang_list = [
-    'bestie',
-    'tea',
-    'cap'
+genz_slang = [
+  'bestie',
+  'tea',
+  'cap',
+  'sus'
 ]
 
-# Iterate over the list and print the slangs
-for term in genz_slang_list:
-    print(term)
+# Printing option 1
+print(genz_slang)
 
-# Define a dictionary with slang terms and meanings
-genz_slang_dict = {
-    "bestie": "best friend or new friend",
-    "tea": "news or gossip"
-}
-
-# Print the slang terms and their meanings
-for term, meaning in genz_slang_dict.items():
-    print(f"{term}: {meaning}")
-
-# Uncomment for alternative example
-    
-# for term, meaning in genz_slang.items():
-    # Print the slang term followed by its meaning
-    # print(term, ":", meaning)
-    # print()  # Add an empty line for readability
-
+# Printing option 2
+for term in genz_slang:
+  print(term)
