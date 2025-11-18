cd1ea34ca6217a052c9910c1c2295f4ba476dd9a
Sonny Li <sonnynomnom@gmail.com>
2025-11-18 15:56:16 -0500

    Update 02_find_my_friends.py

--- 1-data-structures/02_find_my_friends.py
+++ 1-data-structures/02_find_my_friends.py
@@ -1,16 +1,13 @@
 # Find My Friends 🙋🏽‍♀️
 # Codédex
 
-# Task 1: Create Friends' Location Tuples
-friend1_location = (37.7749, -122.4194) # San Francisco
-friend2_location = (40.7128, -74.0060) # New York
+friend1_location = (37.7749, -122.4194)   # San Francisco
+friend2_location = (40.7128, -74.0060)    # New York
 friend3_location = (4.624335, -74.063644) # Bogota
 
 # Bonus: Accessing Tuple Elements
 print("Latitude and Longitude of the furthest friend's location:", friend1_location)
 
-# Task 2: Tuple Operations
 all_friends_locations = friend1_location + friend2_location + friend3_location
 
-# Task 3: Print
 print("All friends' locations combined:", all_friends_locations)
