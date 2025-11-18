43720d014b926ae187c4296089a540f4986314f8
Sonny Li <sonnynomnom@gmail.com>
2025-11-18 17:31:59 -0500

    Update 05_post_game_stats.py

--- 1-data-structures/05_post_game_stats.py
+++ 1-data-structures/05_post_game_stats.py
@@ -3,10 +3,11 @@
 
 # Task 1: Create Player Data
 players_data = [
-    {"name": "Patrick Mahomes", "position": "Quarterback", "jersey_number": 15, "yards_gained": 400, "touchdowns": 3},
-    {"name": "Tyreek Hill", "position": "Wide Receiver", "jersey_number": 10, "yards_gained": 150, "touchdowns": 2},
-    # Add more players as needed
+  {"name": "Patrick Mahomes", "position": "Quarterback", "jersey_number": 15, "yards_gained": 400, "touchdowns": 3},
+  {"name": "Tyreek Hill", "position": "Wide Receiver", "jersey_number": 10, "yards_gained": 150, "touchdowns": 2},
+  # Add more players as needed
 ]
+
 names = [player["name"] for player in players_data]
 print("Players Names:", names)
 
